9e75d41827517ffb5a7dc596054e6f65e940cd47
Huy Ho <huyhoduy@gmail.com>
2026-05-20 23:07:25 +0700

    hien pdf

--- MiniZotero/ViewModels/SidebarViewModel.cs
+++ MiniZotero/ViewModels/SidebarViewModel.cs
@@ -1,17 +1,61 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
 using MiniZotero.Models;
 
 namespace MiniZotero.ViewModels;
 
-public class SidebarViewModel
+public class SidebarViewModel : INotifyPropertyChanged
 {
+    public event PropertyChangedEventHandler? PropertyChanged;
+
     public string SearchText { get; set; } = string.Empty;
 
     public ObservableCollection<DocumentItem> Documents { get; } = new();
 
     public bool HasDocuments => Documents.Count > 0;
 
+    public bool IsDocumentListEmpty => Documents.Count == 0;
+
     public string EmptyTitle { get; } = "No documents yet";
 
     public string EmptyMessage { get; } = "Import a PDF to start";
+
+    public void AddDocumentFromFile(string filePath)
+    {
+        if (string.IsNullOrWhiteSpace(filePath))
+        {
+            return;
+        }
+
+        bool alreadyExists = Documents.Any(document => document.FilePath == filePath);
+
+        if (alreadyExists)
+        {
+            return;
+        }
+
+        var document = new DocumentItem
+        {
+            Id = Guid.NewGuid().ToString(),
+            Title = Path.GetFileNameWithoutExtension(filePath),
+            FilePath = filePath,
+            ImportedAt = DateTime.Now,
+            LastReadPage = 0,
+            TotalPages = 0,
+            IsStarred = false
+        };
+
+        Documents.Add(document);
+
+        OnPropertyChanged(nameof(HasDocuments));
+        OnPropertyChanged(nameof(IsDocumentListEmpty));
+    }
+
+    private void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
--- MiniZotero/Views/SidebarView.axaml.cs
+++ MiniZotero/Views/SidebarView.axaml.cs
@@ -1,11 +1,57 @@
+using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+using MiniZotero.ViewModels;
 
 namespace MiniZotero.Views;
 
 public partial class SidebarView : UserControl
 {
+    private static readonly FilePickerFileType PdfFileType = new("PDF Documents")
+    {
+        Patterns = new[] { "*.pdf" },
+        MimeTypes = new[] { "application/pdf" }
+    };
+
     public SidebarView()
     {
         InitializeComponent();
+    }
+
+    private async void OnImportPdfClicked(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+
+        if (topLevel is null)
+        {
+            return;
+        }
+
+        var files = await topLevel.StorageProvider.OpenFilePickerAsync(
+            new FilePickerOpenOptions
+            {
+                Title = "Import PDF",
+                AllowMultiple = false,
+                FileTypeFilter = new[] { PdfFileType }
+            });
+
+        var selectedFile = files.FirstOrDefault();
+
+        if (selectedFile is null)
+        {
+            return;
+        }
+
+        string? localPath = selectedFile.TryGetLocalPath();
+
+        if (string.IsNullOrWhiteSpace(localPath))
+        {
+            return;
+        }
+
+        if (DataContext is SidebarViewModel viewModel)
+        {
+            viewModel.AddDocumentFromFile(localPath);
+        }
     }
 }
